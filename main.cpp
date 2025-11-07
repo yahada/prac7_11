70c78565de5de377023009012f2b4f57e2030c38
yahada <niadzy14@gmail.com>
2025-11-07 12:44:45 +0300

    program 1.2

--- main.cpp
+++ main.cpp
@@ -13,12 +13,21 @@ int * extend(const int * a, size_t k, size_t d, int filter) {
   return new_arr;
 }
 
+void extend(int **ptr_a, size_t k, size_t d, int fillter) {
+  int * new_arr = extend(*ptr_a, k, d, fillter);
+  delete[] *ptr_a;
+  *ptr_a = new_arr;
+}
+
 
 int main() {
-  int arr[5] = {1, 2, 3, 4, 5};
-  int * new_arr = extend(arr, 5, 10, 0);
+  int * arr = new int[5]{1, 2, 3, 4, 5};
+  int ** ptr_a = &arr;
+  extend(ptr_a, 5, 10, 0);
+  extend(&arr, 5, 10, 0); // &arr - взятие адреса
 
   for (size_t i = 0; i < 10; ++i) {
-    std::cout << new_arr[i] << " ";
+    std::cout << arr[i] << " ";
   }
+  std::cout << "\n";
 }
