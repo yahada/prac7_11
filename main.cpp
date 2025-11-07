6542d61e821814449f494accb5d242d7950b4ac9
yahada <niadzy14@gmail.com>
2025-11-07 13:09:23 +0300

    program 2.2

--- main.cpp
+++ main.cpp
@@ -23,10 +23,33 @@ int * add_row(const int * a, size_t n, size_t m, int filler) {
   return extend(a, n * m, (n+1) * m, filler);
 }
 
+int * add_col(const int * a, size_t n, size_t m, int filler) {
+  int * new_arr = new int[n * (m+1)];
+  for (size_t i = 0; i < n; ++i) {
+    for (size_t j = 0; j < m; ++j) {
+      new_arr[i * (m + 1) + j] = a[i * m + j];
+    }
+    new_arr[i * (m + 1) + m] = filler;
+  }
+  return new_arr;
+}
+
+
+void show_mtx(const int * a, size_t k, size_t d) {
+  for (size_t i = 0; i < k; ++i) {
+    for (size_t j = 0; j < d; ++j) {
+      std::cout << a[i * d + j] << " ";
+    }
+    std::cout << "\n";
+  }
+  std::cout << "\n";
+
+}
 
 int main() {
   int * arr = new int[4]{1, 2, 3, 4};
-  int * new_matrix = add_row(arr, 2, 2, 0);
+  int * new_matrix = add_col(arr, 2, 2, 0);
+  show_mtx( new_matrix, 2, 3);
   // int ** ptr_a = &arr;
   // extend(ptr_a, 5, 10, 0);
   // extend(&arr, 5, 10, 0); // &arr - взятие адреса
@@ -35,12 +58,5 @@ int main() {
   //   std::cout << arr[i] << " ";
   // }
 
-  for (size_t i = 0; i < 3; ++i) {
-    for (size_t j = 0; j < 2; ++j) {
-      std::cout << new_matrix[i * 2 + j] << " ";
-    }
-    std::cout << "\n";
 
-  }
-  std::cout << "\n";
 }
